e807ebfe0c823c7dc66911f38b2d9f8253d2fcb6
luciana-medac <lmg0043@alu.medac.es>
2024-10-07 09:31:46 +0200

    si

---
+++ Tema 3/mas18.java
@@ -0,0 +1,304 @@
+public class mas18 {
+
+    public static void main(String[] args) {
+        
+        
+        int cartasPalo = (int) (Math.random()*13+1);
+
+        switch (cartasPalo) {
+            case 1:
+
+                int palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("A de picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("A de corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("A de diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("A de tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+        
+            case 2:
+                System.out.println("2");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("2 de picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("2 de corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("2 de diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("2 de tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 3:
+                System.out.println("3");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 4:
+                System.out.println("4");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 5:
+                System.out.println("5");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 6:
+                System.out.println("6");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 7:
+                System.out.println("7");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 8:
+                System.out.println("8");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 9:
+                System.out.println("9");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 10:
+                System.out.println("J");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 11:
+                System.out.println("Q");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            case 12:
+                System.out.println("K");
+                palo = (int) (Math.random()*4+1);
+                switch (palo) {
+                    case 1:
+                        System.out.println ("picas") ;
+                        break;
+                
+                    case 2: 
+                        System.out.println ("corazones") ;
+                        break;
+        
+                    case 3: 
+                        System.out.println ("diamantes") ;
+                       break;
+        
+                    case 4: 
+                        System.out.println ("tréboles") ;
+        
+                    default:
+                        break;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
+    
+
