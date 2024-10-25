7a7e00aaec69ebc47cc62a87defa195c7c47bf82
luciana-medac <lmg0043@alu.medac.es>
2024-10-25 08:50:45 +0200

    Update Ejercicio39.java

--- Tema 3/Ejercicio39.java
+++ Tema 3/Ejercicio39.java
@@ -1,8 +1,8 @@
 public class Ejercicio39 {
 
     /*
-     * Crea un programa que pregunte al usuario si quiere apagar el sistema con la pregunta "¿Seguro que quieres apagar el sistema? (s/n)". Usa un bucle para seguir pidiendo al usuario la respuesta repetidamente.
-
+     * Crea un programa que pregunte al usuario si quiere apagar el sistema con la pregunta "¿Seguro que quieres apagar el sistema? (s/n)". 
+     * Usa un bucle para seguir pidiendo al usuario la respuesta repetidamente.
     Si introduce una 's', se acaba el programa.
     Si introduce una 'n' se muestra el mensaje 'el programa continua...' y sigue preguntando.
     Si introduce cualquier otra cosa se muestra el mensaje 'respuesta no válida' y sigue preguntando.
