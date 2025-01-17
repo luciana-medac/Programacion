4e1a2e7bd96356182ee9afbea8c3353753aeb390
luciana-medac <lmg0043@alu.medac.es>
2025-01-17 11:16:34 +0100

    si

--- Segundo_Trimestre/Ejercicio17/src/main/java/com/mycompany/ejercicio17/Ejercicio17.java
+++ Segundo_Trimestre/Ejercicio17/src/main/java/com/mycompany/ejercicio17/Ejercicio17.java
@@ -1,16 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.ejercicio17;
 
-/**
- *
- * @author PC218
- */
+/*
+CLASES ESPECIALES: STATIC (ESTATICAS) Y LAS QUE TIENEN UN ENUM
+*/
 public class Ejercicio17 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+        
     }
 }
