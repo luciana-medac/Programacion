ac3f77b76f68d14b9327b4671300759051c2c6e3
lachthachi <lmg0043@alu.medac.es>
2025-02-03 14:26:48 +0100

    circulo

--- Segundo_Trimestre/T10T11_OrientadoObjetos/Ejercicio6/src/main/java/com/mycompany/ejercicio6/Figura.java
+++ Segundo_Trimestre/T10T11_OrientadoObjetos/Ejercicio6/src/main/java/com/mycompany/ejercicio6/Figura.java
@@ -10,7 +10,7 @@ public class Figura {
         
         System.out.println("calculamos el area");
         
-        //PARA PONER EL NÚMERO PI --> Math.PI
+        //PARA PONER EL NÚMERO PI --> Math.PI * Math.pow(radio, 2) primero el núm y después el exponente
         
     }
     
